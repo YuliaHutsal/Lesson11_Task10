ea1904b867949f4edad676ff7be13083fc9f9c0a
Yulia Gutsal <julik.ua@gmail.com>
2023-01-03 14:19:23 +0200

    Change Modal file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Modal from './Components/Modal';
+import { useState } from 'react';
+
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="App">
-      
+      {modalOpen && <Modal />}
+      <h2>Click the button</h2>
+      <button
+        className="openModalBtn"
+        onClick={() => setModalOpen(true)}
+      >
+        Open
+      </button>
     </div>
   );
 }
---
+++ src/Components/Modal.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './Modal.css';
+
+export default function Modal({setOpenModal}){
+    return(
+        <div className='modalBackground'>
+            <div className='modelContainer'>
+                <div className='titleCloseBtn'>
+                    <button
+                    onClick={() => {
+                        setOpenModal(false)
+                    }}
+                    >
+                        X
+                    </button>
+
+                </div>
+                <div className="title">
+                    <h2>Are you sure you want to continue?</h2>
+                </div>
+                <div className="body">
+                    <p>The next page looks amazing!</p>
+
+                </div>
+                <div className="footer">
+                    <button
+                        onClick={() => {setOpenModal(false);
+                        }}
+                        id = 'cancelBtn'
+                        >
+                            Cancel
+                    </button>
+                    <button>Continue</button>
+
+                </div>
+
+            </div>
+        </div>
+    );
+}
